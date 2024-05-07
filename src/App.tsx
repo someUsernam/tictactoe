a7f8d7265cfb6b7b33a34f88e429c4cedb508966
Artur Wojtowicz <arturwojtowik@gmail.com>
2024-05-07 16:38:06 +0200

    refactor: Update imports and file structure for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled, { createGlobalStyle } from "styled-components";
-import { GameContainer } from "./modules/gameContainer";
+import { GameContainer } from "./pages/gameContainer";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
---
+++ src/common/consts/Links.ts
@@ -0,0 +1,6 @@
+export const Links = {
+  home: '/',
+  'create-players': '/create-players',
+  'game-options': '/game-options',
+  game: '/game',
+}
